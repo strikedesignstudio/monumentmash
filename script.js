475782727026717acf7cc5cb45bfb0161e82c014
longhousetohighrise <89917147+strikedesignstudio@users.noreply.github.com>
2026-02-05 10:27:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -292,7 +292,12 @@ const hoverColorMap = {
     "Jessica": 0x0088ff,
     "Rachid": 0x8800ff,
     "Jermaine": 0xff0088,
-    "Dominic": 0x00ff88
+    "Dominic": 0x00ff88,
+    "Mesh001": 0xff6b6b,  // Coral red
+    "Mesh002": 0x4ecdc4,  // Turquoise
+    "Mesh003": 0xffe66d,  // Yellow
+    "Mesh004": 0xa8e6cf,  // Mint green
+    "Mesh005": 0xff8b94   // Pink
 };
 
 const playPauseBtn = document.getElementById('play-pause-btn');
