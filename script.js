171b9cf266eeeba52653813bb47e166274ffac38
longhousetohighrise <89917147+strikedesignstudio@users.noreply.github.com>
2026-02-05 16:21:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1021,7 +1021,7 @@ sceneLayer.addEventListener("mousemove", onMouseMove);
 const loader = new THREE.GLTFLoader();
 
 loader.load(
-    'model/modelfull2.glb',
+    'model/modelfull3.glb',
     function (gltf) {
         loadingProgress.model = 100;
         updateLoadingBar();
