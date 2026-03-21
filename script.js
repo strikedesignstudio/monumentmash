fc30f851a2430f98c51155910fe9f3e495987198
longhousetohighrise <89917147+strikedesignstudio@users.noreply.github.com>
2026-03-21 18:24:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1207,7 +1207,7 @@ child.position.x = radius * Math.sin(phi) * Math.cos(theta);
 child.position.y = radius * Math.sin(phi) * Math.sin(theta) - 2.5;
 child.position.z = 30 + Math.random() * 100; // forward bias in local space
                         
-                        child.scale.multiplyScalar(3);
+                        child.scale.multiplyScalar(2);
                         child.userData.velocityX = (Math.random() - 0.5) * 0.02;
                         child.userData.velocityY = (Math.random() - 0.5) * 0.02;
                         child.userData.velocityZ = (Math.random() - 0.5) * 0.02;
